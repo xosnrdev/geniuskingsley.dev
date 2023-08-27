a24de6603279c3a30bb751fd0c8f6452e17da0b8
spadechaser <queenmaker@geniuskingsley.dev>
2023-08-27 17:21:11 -0400

    --fix

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,20 +1,19 @@
 /* eslint-disable react/display-name */
-import { coreContent } from '@/lib/utils/contentlayer';
-import type { Authors, Blog } from 'contentlayer/generated';
-import { ComponentMap } from 'mdx-bundler/client';
-import { useMDXComponent } from 'next-contentlayer/hooks';
-import Image from './Image';
-import CustomLink from './Link';
-import LinkButton from './LinkButton';
-import Pre from './Pre';
-import TOCInline from './TOCInline';
+import { coreContent } from "@/lib/utils/contentlayer";
+import type { Authors, Blog } from "contentlayer/generated";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import Image from "./Image";
+import CustomLink from "./Link";
+import LinkButton from "./LinkButton";
+import Pre from "./Pre";
+import TOCInline from "./TOCInline";
 
 interface MDXLayout {
   content: Blog | Authors;
   [key: string]: unknown;
 }
 
-export const MDXComponents: ComponentMap = {
+export const MDXComponents = {
   Image,
   TOCInline,
   a: CustomLink,
