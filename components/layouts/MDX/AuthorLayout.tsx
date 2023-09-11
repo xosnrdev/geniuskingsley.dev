ce5f9c2a8d2839d0ca5136d707cc3099ba456a62
spadechaser <queenmaker@geniuskingsley.dev>
2023-09-11 15:31:38 -0400

    resume integration

--- components/layouts/MDX/AuthorLayout.tsx
+++ components/layouts/MDX/AuthorLayout.tsx
@@ -17,7 +17,7 @@ export default function AuthorLayout({ children, content }: Props) {
     <div className="m-auto pt-8">
       <div className="my-2 flex flex-col-reverse items-center justify-between sm:flex-row">
         <div className="text-clip text-center sm:text-left">
-          <h1 className="font-cabinet text-xl font-extrabold text-blue-600 md:text-3xl lg:text-4xl">
+          <h1 className="font-cabinet text-xl font-extrabold text-blue-500 md:text-3xl lg:text-4xl">
             {name}
           </h1>
           <h2 className="self-stretch text-sm font-normal capitalize tracking-normal md:text-base">
@@ -26,10 +26,10 @@ export default function AuthorLayout({ children, content }: Props) {
           <div className="my-2 flex flex-col gap-y-2 text-center">
             <div className=" flex items-center">
               <Link
-                href="/"
+                href="https://drive.google.com/file/d/1RuRj5rHM-Qg32oxeVKT9-C-ot3lpl3EM/view?usp=sharing"
                 rel="noreferrer noopener"
                 target="_blank"
-                className="flex basis-[90%] items-center justify-center rounded-md bg-blue-600 text-center font-cabinet text-lg font-semibold text-slate-100"
+                className="flex basis-[90%] items-center justify-center rounded-md border-2 border-blue-500 text-center font-cabinet text-lg font-semibold hover:bg-blue-500 hover:text-slate-100"
               >
                 View Resume{" "}
                 <svg
@@ -49,7 +49,7 @@ export default function AuthorLayout({ children, content }: Props) {
             </div>
           </div>
         </div>
-        <div className="pointer-events-none rounded-full border-4 border-blue-600">
+        <div className="pointer-events-none rounded-full border-4 border-blue-500">
           <Image
             alt="Chidera Kingsley"
             height={130}
@@ -59,26 +59,6 @@ export default function AuthorLayout({ children, content }: Props) {
           />
         </div>
       </div>
-      {/* <span className="my-2 flex items-center justify-center lg:hidden">
-        <Link
-          href="mailto:kharunaking@gmail.com"
-          className="css-1tzfalu flex items-center justify-center gap-x-2"
-        >
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            stroke-width="0"
-            viewBox="0 0 24 24"
-            className="object-center text-lg"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M20 4H4c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2zm0 2v.511l-8 6.223-8-6.222V6h16zM4 18V9.044l7.386 5.745a.994.994 0 0 0 1.228 0L20 9.044 20.002 18H4z"></path>
-          </svg>
-          Send me an email
-        </Link>
-      </span> */}
       <div className="prose max-w-none self-stretch pb-8 font-normal normal-case tracking-normal dark:prose-dark md:text-lg xl:col-span-2">
         {children}
       </div>
